581bef2c483a5fde85661d152189f9152ffd6b0a
pewww <jung22337@naver.com>
2021-04-11 22:47:40 +0900

    feat: 모듈 예제 추가

---
+++ advanced/mod.py
@@ -0,0 +1,3 @@
+add = lambda num1, num2: num1 + num2
+
+sub = lambda num1, num2: num1 - num2
---
+++ advanced/module.py
@@ -0,0 +1,8 @@
+from mod import add, sub
+
+print(add(1, 3))
+print(sub(5, 2))
+
+from mod2 import Math
+
+print(Math().solv(2))
